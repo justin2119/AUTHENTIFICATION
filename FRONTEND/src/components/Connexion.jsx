dc5d765d8354ce951af81375ca198ed82369fff4
draoude21 <201861475+justin2119@users.noreply.github.com>
2025-09-14 19:57:19 +0000

    Authentification
    Athentification Reuissi

--- FRONTEND/src/components/Connexion.jsx
+++ FRONTEND/src/components/Connexion.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from 'react'
 import {Link, useNavigate} from 'react-router'
+import axios from "axios"
 
 const Login = () => {
   const navigate=useNavigate()
-  const handleSubmit=()=>{
-    navigate("acceuil")
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    axios.post("http://localhost:3000/api/signup",{email,password})
+    .then(res=>{
+      console.log(res)
+      navigate("acceuil")
+    })
+    .catch(err=>console.log(err))
+
   }
   return (
     <div className="w-full h-screen container ">
@@ -12,9 +22,9 @@ const Login = () => {
       <marquee className=" text-2xl font-light text-blue-600 text-center mb-3">Bienvenus pour la connexion</marquee>
         <form action="" onSubmit={handleSubmit}>
           <label className="w-full text-xl  font-light mb-1" htmlFor="email">Adresse email</label> <br />
-          <input className="border-2 w-full py-2  px-2 rounded-xl" type="email" name="email" id="" placeholder="Email" /> <br />
-          <label className="w-full text-xl font-light mb-1" htmlFor="username" >Mots de passe</label> <br />
-          <input className="border-2 w-full py-2  px-2 rounded-xl" type="text" name="password" id="" placeholder="123@hhhhh" /> <br />
+          <input value={email} onChange={(e)=>setEmail(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="email" name="email" id="" placeholder="Email" /> <br />
+          <label className="w-full text-xl font-light mb-1" htmlFor="password" >Mots de passe</label> <br />
+          <input minLength={4} maxLength={8} value={password} onChange={(e)=>setPassword(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="password" name="password" id="" placeholder="123@hhhhh" /> <br />
           <input className="bg-blue-700 mt-2 px-2 py-3 w-full text-white font-light rounded-xl" type="submit" value={"Connexion"} />
         </form>
         <Link to="/inscription" className="float-end text-md font-normal underline font-light italic" >Creer un compte</Link>
--- FRONTEND/src/components/Inscription.jsx
+++ FRONTEND/src/components/Inscription.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
+import axios from "axios"
 
 
 
 const Inscription = () => {
   const navigate =useNavigate()
+  const[nom,setNom]=useState("")
+  const[prenom,setPrenom]=useState("")
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const[confirm_password,setConfirm_password]=useState("")
   
-  function Submite(){
-    navigate("/")
+  const Submit=(e)=>{
+    e.preventDefault()
+    axios.post("http://localhost:3000/api/login",{nom,prenom,email,password,confirm_password})
+          .then(res=>{
+            console.log(res.data)
+            navigate("/")
+          })
+          .catch(err=>console.log(err))
   }
   return (
-    <div className="w-full h-screen container ">
-      <div className="w-1/2 mx-auto translate-y-[50%]">
+    <div className="w-full h-full container ">
+      <div className="w-1/2 mx-auto h-1/2 mt-5">
       <marquee className=" text-2xl font-light text-blue-600 text-center mb-3">Bienvenus pour l'inscription</marquee>
-        <form action="" onSubmit={Submite}>
+        <form action="" onSubmit={Submit}>
           <label className="w-full text-xl  font-light mb-1" htmlFor="nom">Nom</label> <br />
-          <input className="border-2 w-full py-2  px-2 rounded-xl" type="text" name="nom" id="" placeholder="Nom" /> <br />
+          <input value={nom} onChange={(e)=>setNom(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="text" name="nom" id="" placeholder="Nom" /> <br />
+          <label className="w-full text-xl  font-light mb-1" htmlFor="nom">Prenom</label> <br />
+          <input value={prenom} onChange={(e)=>setPrenom(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="text" name="prenom" id="" placeholder="Prenom" /> <br />
           <label className="w-full text-xl  font-light mb-1" htmlFor="email">Adresse email</label> <br />
-          <input className="border-2 w-full py-2  px-2 rounded-xl" type="email" name="email" id="" placeholder="Email" /> <br />
+          <input value={email} onChange={(e)=>setEmail(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="email" name="email" id="" placeholder="Email" /> <br />
           <label className="w-full text-xl font-light mb-1" htmlFor="password" >Mots de passe</label> <br />
-          <input className="border-2 w-full py-2  px-2 rounded-xl" type="password" name="password" id="" placeholder="123@hhhhh" /> <br />
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="password" name="password" id="" placeholder="123@hhhhh" /> <br />
+           <label className="w-full text-xl font-light mb-1" htmlFor="confirm_password" > Confirmer le mots de passe</label> <br />
+          <input value={confirm_password} onChange={(e)=>setConfirm_password(e.target.value)} className="border-2 w-full py-2  px-2 rounded-xl" type="password" name="confirm_password" id="" placeholder="123@hhhhh" /> <br />
           <input className="bg-blue-700 mt-2 px-2 py-3 w-full text-white font-light rounded-xl" type="submit" value={"Inscription"} />
         </form>
       </div>
